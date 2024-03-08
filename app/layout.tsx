78d03f13fc76576abe839d29b1d14c809d562753
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2024-03-07 20:08:47 -0500

    proyecto con next js

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center'>Hecho con nextJs 😎</footer>
+        </body>
+    </html>
+  );
+}
